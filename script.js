3844596f17fa175ab5f660d78b1217856cbaf220
Natalie McDowell <nhamilton02@qub.ac.uk>
2026-04-19 18:51:05 +0100

    Added markers and click events

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+// Add a marker at a specific location
+var marker = L.marker([51.5, -0.09]).addTo(map);
+
+// Add a popup to that marker
+marker.bindPopup("<b>Hello World!</b><br>This is a custom popup.").openPopup();
+
+// Create a blank popup
+var popup = L.popup();
+
+// Function to handle clicks
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+// Tell the map to listen for clicks
+map.on('click', onMapClick);
