ee0016030626e4c70fe8fb254f1a5a17c29c3946
Natalie McDowell <nhamilton02@qub.ac.uk>
2026-04-23 17:04:57 +0100

    Changed colour of polygon

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 // [Latitude, Longitude], Zoom Level
 var map = L.map('map').setView([54.597, -5.930], 15);
 
-// Add a tile layer (the map images). We use OpenStreetMap.
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+// Add a tile layer (the map images). We use OpenStreetMap. Changed to "Humanitarian Style".
+L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
     maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    attribution: '© OpenStreetMap contributors, Humanitarian OpenStreetMap Team'
 }).addTo(map);
 
 // Add a marker at a specific location
@@ -27,3 +27,35 @@ function onMapClick(e) {
 
 // Tell the map to listen for clicks
 map.on('click', onMapClick);
+
+var circle = L.circle([54.597, -5.930], {
+    color: 'red',       // The color of the border
+    fillColor: '#f03',  // The color of the inside
+    fillOpacity: 0.5,   // How see-through it is (0 to 1)
+    radius: 1000         // Radius in meters
+}).addTo(map);
+
+circle.bindPopup("I am a 500 meter circle centered on Belfast.");
+
+var polygon = L.polygon([
+    [54.600, -5.935],
+    [54.605, -5.930],
+    [54.600, -5.925],
+], {color: 'green',
+    fillColor: 'green',
+    fillOpacity: 0.5,
+    }
+).addTo(map);
+
+polygon.bindPopup("I am a polygon (triangle).");
+
+// Put your features into a group
+var myFeatures = L.layerGroup([marker, circle, polygon]);
+
+// Create an object to name the group in the menu
+var overlayMaps = {
+    "My Interesting Places": myFeatures
+};
+
+// Add the control to the map
+L.control.layers(null, overlayMaps).addTo(map);
