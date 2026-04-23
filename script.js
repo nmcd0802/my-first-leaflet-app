ee528f5c3c9329f515f422dbf1773e1e6649be7d
Natalie McDowell <nhamilton02@qub.ac.uk>
2026-04-23 17:31:35 +0100

    Added QUB marker and click options

--- script.js
+++ script.js
@@ -19,6 +19,10 @@ var map = L.map('map', {
 var marker = L.marker([54.5965, -5.9301]).addTo(map);
 marker.bindPopup("<b>Belfast City Hall</b>");
 
+// Add a marker for QUB Lanyon Building
+var qubMarker = L.marker([54.5847, -5.9348]).addTo(map);
+qubMarker.bindPopup("<b>QUB Lanyon Building</b><br>Designed by Sir Charles Lanyon.");
+
 var circle = L.circle([54.597, -5.930], {
     color: 'red',
     radius: 1000
@@ -31,7 +35,21 @@ var baseMaps = {
 };
 
 var overlayMaps = {
-    "My Interesting Places": L.layerGroup([marker, circle])
+    "My Interesting Places": L.layerGroup([marker, circle, qubMarker])
 };
 
 L.control.layers(baseMaps, overlayMaps).addTo(map);
+
+// Create a blank popup
+var popup = L.popup();
+
+// Function to handle clicks
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+// Tell the map to listen for clicks
+map.on('click', onMapClick);
