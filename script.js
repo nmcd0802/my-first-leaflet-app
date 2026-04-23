3c44d46f603daae21af424b1e332207cf18d3e16
Natalie McDowell <nhamilton02@qub.ac.uk>
2026-04-23 17:23:54 +0100

    Added satellite layer and tidied code

--- script.js
+++ script.js
@@ -1,61 +1,37 @@
-// Initialize the map and set its view to a chosen geographical point and zoom
-// [Latitude, Longitude], Zoom Level
-var map = L.map('map').setView([54.597, -5.930], 15);
-
-// Add a tile layer (the map images). We use OpenStreetMap. Changed to "Humanitarian Style".
-L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+// 1. Define the background layers first
+var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
-    attribution: '© OpenStreetMap contributors, Humanitarian OpenStreetMap Team'
-}).addTo(map);
+    attribution: '© OpenStreetMap'
+});
 
-// Add a marker at a specific location
+var satellite = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri &mdash; Source: Esri'
+});
+
+// 2. Initialize the map using one of those layers
+var map = L.map('map', {
+    center: [54.597, -5.930],
+    zoom: 13,
+    layers: [osm] // Default layer
+});
+
+// 3. Add your shapes and markers
 var marker = L.marker([54.5965, -5.9301]).addTo(map);
-
-// Add a popup to that marker
-marker.bindPopup("<b>Belfast City Hall</b><br>The heart of the city.").openPopup();
-
-// Create a blank popup
-var popup = L.popup();
-
-// Function to handle clicks
-function onMapClick(e) {
-    popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(map);
-}
-
-// Tell the map to listen for clicks
-map.on('click', onMapClick);
+marker.bindPopup("<b>Belfast City Hall</b>");
 
 var circle = L.circle([54.597, -5.930], {
-    color: 'red',       // The color of the border
-    fillColor: '#f03',  // The color of the inside
-    fillOpacity: 0.5,   // How see-through it is (0 to 1)
-    radius: 1000         // Radius in meters
+    color: 'red',
+    radius: 1000
 }).addTo(map);
 
-circle.bindPopup("I am a 500 meter circle centered on Belfast.");
-
-var polygon = L.polygon([
-    [54.600, -5.935],
-    [54.605, -5.930],
-    [54.600, -5.925],
-], {color: 'green',
-    fillColor: 'green',
-    fillOpacity: 0.5,
-    }
-).addTo(map);
-
-polygon.bindPopup("I am a polygon (triangle).");
-
-// Put your features into a group
-var myFeatures = L.layerGroup([marker, circle, polygon]);
-
-// Create an object to name the group in the menu
-var overlayMaps = {
-    "My Interesting Places": myFeatures
+// 4. Setup the Layer Control
+var baseMaps = {
+    "Street Map": osm,
+    "Satellite": satellite
 };
 
-// Add the control to the map
-L.control.layers(null, overlayMaps).addTo(map);
+var overlayMaps = {
+    "My Interesting Places": L.layerGroup([marker, circle])
+};
+
+L.control.layers(baseMaps, overlayMaps).addTo(map);
